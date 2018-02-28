e9cf2e019829944bfa1ae1ffe45d1f2968962053
Mio <a771644612@live.com>
2018-02-28 22:00:50 +0800

    添加了bits转bytes的函数

--- adwtmk/utilities.py
+++ adwtmk/utilities.py
@@ -1,3 +1,7 @@
+from array import array
+from functools import reduce
+
+
 def get_all_bits(byte_string: bytes, reverse: bool=False)->list:
     if not reverse:
         return [bit
@@ -7,3 +11,11 @@ def get_all_bits(byte_string: bytes, reverse: bool=False)->list:
         return [bit
                 for L in map(lambda byte: [((byte & (1 << i)) >> i) for i in range(8)], byte_string)
                 for bit in L]
+
+
+def get_all_bytes(byte_list: list)->bytes:
+    decoded_bytes = array('B', [])
+    bytes_len = len(byte_list) // 8
+    for i in range(bytes_len):
+        decoded_bytes.append(int(reduce(lambda x, y: (x | y) << 1, byte_list[8 * i:8 * i + 8]) >> 1))
+    return decoded_bytes.tobytes()
