9afd0149861a4b6875fb1e099df16190dc827069
Mio <a771644612@live.com>
2018-02-28 21:36:44 +0800

    把检测key合理性的重复代码写成了装饰器

--- adwtmk/decoder.py
+++ adwtmk/decoder.py
@@ -15,6 +15,24 @@ class KeyNotFoundError(WaterMarkDecodeError):
     pass
 
 
+def dectect_key(key_type: str):
+    def detect(func):
+        def wrapper(marked_audio: Audio, key: dict)->bytes:
+            if marked_audio.key is None and key is None:
+                raise KeyNotFoundError("No key found.")
+            if marked_audio.key is None or marked_audio.key.get("type", None) != key_type and \
+               key is None or key.get("type", None) != key_type:
+                raise WrongKeyError("Wrong type of key.")
+            if marked_audio.key.get("type", None) == key_type:
+                key = marked_audio.key
+            if key.get('key', None) is None:
+                raise KeyNotFoundError("No key found.")
+            return func(marked_audio, key)
+        return wrapper
+    return detect
+
+
+@dectect_key("LSB")
 def lsb_decode(marked_audio: Audio, key: dict=None)->bytes:
     """
     用Audio.key或者传入的key对音频解LSB码，返回解码所得的bytes。
@@ -23,15 +41,7 @@ def lsb_decode(marked_audio: Audio, key: dict=None)->bytes:
     :param key: （可选）解码用的key，**如果Audio.key是有效的，这个参数会被忽略。**
     :return: 隐写的bytes。
     """
-    if marked_audio.key is None and key is None:
-        raise KeyNotFoundError("No key found.")
-    if marked_audio.key.get("type", None) != "LSB" and key.get("type", None) != "LSB":
-        raise WrongKeyError("Wrong type of key.")
-    if marked_audio.key.get("type", None) == "LSB":
-        key = marked_audio.key
-    key_list = key.get('key', None)
-    if not key_list:
-        raise KeyNotFoundError("No key found.")
+    key_list = key['key']
     if not isinstance(key_list, list):
         raise WrongKeyError("Wrong type of key.")
     if len(key_list) % 8 != 0:
