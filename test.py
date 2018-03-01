8b2a1c6bc6ae844876c6c8be54510718d455ead0
Mio <a771644612@live.com>
2018-03-01 14:09:00 +0800

    添加了完整的test和注释

--- test.py
+++ test.py
@@ -1,16 +1,33 @@
-from pydub import AudioSegment
-from adwtmk.audio import Audio
 from adwtmk.encoder import *
 from adwtmk.decoder import *
 
-sound1 = Audio.from_file("./mark.flac", format="flac")
+# read sound file
+sound = Audio.from_file("./mark.flac", format="flac")
 
-print(sound1.frame_rate,sound1.duration_seconds, len(sound1.get_array_of_samples()))
+# basic information
+print(sound.frame_rate, sound.duration_seconds, len(sound.get_array_of_samples()))
 
-print(sound1.get_reshaped_samples())
+# metadata
+print(sound.tags)
 
-marked = lsb_encode(sound1, b"woccccccc")
+# same as audioread in matlab
+print(sound.get_reshaped_samples())
 
-marked.export_with_key(key_path="./test/key.json", out_path="./test/mark.flac", format="flac", tags=marked.tags)
+# LSB encoding
+LSB_marked = lsb_encode(sound, b"LSB")
 
-print(lsb_decode(marked))
+# LSB decoding
+print(lsb_decode(LSB_marked))
+
+# echo encoding
+ECHO_marked = echo_encode(sound, b'ECHO')
+
+# echo decoding
+print(echo_decode(ECHO_marked))
+
+# spawn a new audio like audiowrite in matlab
+samples = ECHO_marked.get_reshaped_samples()
+new_audio = ECHO_marked.spawn(samples)
+
+# and export it with key
+new_audio.export_with_key(key_path="./test/key.json", out_path="./test/mark.flac", format="flac", tags=ECHO_marked.tags)
