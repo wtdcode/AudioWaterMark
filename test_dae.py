9f284ad0b3beb44aa7af807e18a8dac15f693a13
chrome01 <1049909407@qq.com>
2018-03-05 01:26:04 +0800

    fix comment bug

--- test_dae.py
+++ test_dae.py
@@ -10,7 +10,7 @@ dae = DAE("./Neural_sign.ckpt")
 # recommend to train the model in another thread in order not to crush the UI thread.
 try:
     t = Thread(target=dae.slow_training,args=(lsb_marked,))
-    #t.start()
+    t.start()
     # The exact neural sign is the file , the name of which begins with "Neural_sign.ckpt"
     # you can use fast/slow/medium_training, fast - high speed but most noisy
 except:
@@ -22,11 +22,11 @@ except:
 print(dae.get_current_training_process())
 try:
     #syncronize()
-    #t.join()
+    t.join()
     # The test_sound is the sound that suffer from the cutting attack, and the output_sound is the denoised sound
     test_sound,output_sound = dae.test(sound) 
     #To get mathematically exact test loss for the sound, remember to call dae.test(sound) firstly
-    print("attack error:%f, output error rate:%f"%(dae.origin_loss,dae.loss))
+    print("attack error:%f, output error rate:%f"%(dae.original_loss,dae.loss))
 except Exception as e:
     print("During the test:")
     print(e)
